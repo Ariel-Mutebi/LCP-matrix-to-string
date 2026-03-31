eea0ab0f5e8c974286686fe4ca85bb9c1bfbc531
black-RAM <pplanes311@gmail.com>
2026-03-31 09:27:35 +0300

    Claude-generated union find solution which I'm trying to understand

--- main.py
+++ main.py
@@ -1,148 +1,65 @@
-# I decided to John Carmack up and not ask AI this time.
 from collections import defaultdict
 from string import ascii_lowercase
 
+
+def find_components(n: int, edges: list[tuple[int, int]]) -> list[int]:
+    """Returns a list mapping each node to its component representative."""
+    parent = list(range(n))
+
+    def find(x: int) -> int:
+        while parent[x] != x:
+            parent[x] = parent[parent[x]]
+            x = parent[x]
+        return x
+
+    def union(x: int, y: int):
+        parent[find(x)] = find(y)
+
+    for a, b in edges:
+        union(a, b)
+
+    return [find(i) for i in range(n)]
+
+
 def reconstruct_matrix(s: str) -> list[list[int]]:
     n = len(s)
-    matrix: list[list[int | None]] = [[None for _ in range(n)] for _ in range(n)]
-
-    for i in range(0, n):
-        ss1 = s[i:n]
-        for j in range(0, n):
-            if not matrix[i][j] is None:
-                continue
-
-            ss2 = s[j:n]
-            shorter = ss1 if len(ss1) < len(ss2) else ss2
-            longer = ss2 if len(ss2) > len(ss1) else ss1
+    matrix = [[0] * n for _ in range(n)]
+    for i in range(n):
+        for j in range(i, n):
             k = 0
-
-            while k < len(shorter):
-                if not shorter[k] == longer[k]:
-                    break
+            while i + k < n and j + k < n and s[i + k] == s[j + k]:
                 k += 1
-
-            matrix[i][j] = k
-            # An LCP matrix is symmetrical along the diagonal line
-            # from top left to bottom right (line where i == j).
-            if not i == j:
-                matrix[j][i] = k
-
+            matrix[i][j] = matrix[j][i] = k
     return matrix
 
-def generate_equal_char_indexes(matrix: list[list[int]]) -> list[list[int]]:
-    n = len(matrix)
-
-    # I decided to use an undirected adjacency list to represent indexes of
-    # equal characters, where a path is a sequence of indexes of equal characters.
-    indexes_of_equal_chars = defaultdict(list)
-
-    # These ranges will be "sublimated" into individual indexes as more
-    # is learned from iterating through the "signal" part of the matrix.
-    # The key of the list of RangePairs is the number of characters in each range.
-    Range = tuple[int, int]
-    RangePair = tuple[Range, Range]
-    ranges_of_equal_chars: dict[int, list[RangePair]] = defaultdict(list)
-
-    # The "signal" part of the matrix is the top-right half above the diagonal line.
-    # The bottom-left half is a reflection.
-    for i in range(0, n):
-        for j in range(i, n):
-            lcp = matrix[i][j]
-            if lcp <= 0:
-                continue
-
-            if lcp == 1:
-                indexes_of_equal_chars[i].append(j)
-                indexes_of_equal_chars[j].append(i)
-
-                # sublimate relevant two-character ranges
-                if ranges_of_equal_chars[2]:
-                    for pair_of_range_tuples in ranges_of_equal_chars[2]:
-                        (a, b), (c, d) = pair_of_range_tuples
-                        sublimated = False
-
-                        if i == a and j == c:
-                            indexes_of_equal_chars[b].append(d)
-                            indexes_of_equal_chars[d].append(b)
-                            sublimated = True
-                        elif i == b and j == d:
-                            indexes_of_equal_chars[a].append(c)
-                            indexes_of_equal_chars[c].append(a)
-                            sublimated = True
-
-                        if sublimated:
-                            ranges_of_equal_chars[2].remove(pair_of_range_tuples)
-            else:
-                if lcp > n - i or lcp > n - j:
-                    raise ValueError("LCP out of range")
-
-                new_ranges = ((i, i + lcp - 1), (j, j + lcp - 1))
-                ranges_of_equal_chars[lcp].append(new_ranges)
-
-                # sublimate relevant ranges of +1 size
-                if (lcp + 1) in ranges_of_equal_chars:
-                    for pair_of_range_tuples in ranges_of_equal_chars[lcp + 1]:
-                        (a, b), (c, d) = pair_of_range_tuples
-                        (e, f), (g, h) = new_ranges  # singing the alphabet song
-                        sublimated = False
-
-                        if a == e and c == g:
-                            indexes_of_equal_chars[b].append(d)
-                            indexes_of_equal_chars[d].append(b)
-                            sublimated = True
-                        elif b == f and d == h:
-                            indexes_of_equal_chars[a].append(c)
-                            indexes_of_equal_chars[c].append(a)
-                            sublimated = True
-
-                        if sublimated:
-                            ranges_of_equal_chars[lcp + 1].remove(pair_of_range_tuples)
-
-    def find_components(graph: dict[int, list[int]]) -> list[list[int]]:
-        visited: set[int] = set()
-        components: list[list[int]] = []
-
-        def dfs(node: int, component: list[int]):
-            visited.add(node)
-            component.append(node)
-
-            for neighbour in graph[node]:
-                if neighbour not in visited:
-                    dfs(neighbour, component)
-
-        for node in graph:
-            if node not in visited:
-                component = []
-                dfs(node, component)
-                components.append(component)
-
-        return components
-
-    return find_components(indexes_of_equal_chars)
 
 class Solution:
-    def findTheString(self, matrix: list[list[int]]) -> str:
-        try:
-            groups_of_equal_char_indexes = generate_equal_char_indexes(matrix)
-        except ValueError:
-            return ""
+    def findTheString(self, lcp: list[list[int]]) -> str:
+        n = len(lcp)
 
-        # to minimise lexicographical size.
-        groups_of_equal_char_indexes.sort(key=min)
-        letters: list[str | None] = [None for _ in range(len(matrix))]
+        # Collect union constraints: lcp[i][j] >= k means char[i+k] == char[j+k]
+        edges = []
+        for i in range(n):
+            for j in range(i + 1, n):
+                for k in range(lcp[i][j]):
+                    if i + k >= n or j + k >= n:
+                        return ""  # lcp value is out of range
+                    edges.append((i + k, j + k))
 
-        for i in range(0, len(groups_of_equal_char_indexes)):
-            for index in groups_of_equal_char_indexes[i]:
-                letters[index] = ascii_lowercase[i % 26]
+        # Find connected components; assign letters in order of first appearance
+        component = find_components(n, edges)
+        rep_to_letter: dict[int, str] = {}
+        letter_idx = 0
+        result = []
 
-        # filter in case input matrix has borders of zeros
-        word = "".join(filter(None, letters))
+        for i in range(n):
+            rep = component[i]
+            if rep not in rep_to_letter:
+                if letter_idx >= 26:
+                    return ""  # more unique chars than alphabet allows
+                rep_to_letter[rep] = ascii_lowercase[letter_idx]
+                letter_idx += 1
+            result.append(rep_to_letter[rep])
 
-        # verify that original LCP matrix was symmetrical and had no zero borders
-        if matrix == reconstruct_matrix(word):
-            return word
-        else:
-            return ""
-
-solution = Solution()
+        word = "".join(result)
+        return word if reconstruct_matrix(word) == lcp else ""
